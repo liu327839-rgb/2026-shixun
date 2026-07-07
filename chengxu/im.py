9ece53bbe4060a4e9ecfab4666df6ad1ae126edd
ditto258 <2418515797@qq.com>
2026-07-07 16:04:33 +0800

    Update im.py

---
+++ chengxu/im.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+def process(img_path):
+    img=cv2.imread(img_path)
+    img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    img=cv2.resive(img,(224,224))
+    img=img.astype(np.float32)/255.0
+    img=(img-[0.485,0.456,0.406])/[0.229,0.224,0.225]
+    return img.transpose(2,0,1)[np.newaxis,...]
+
+
+    
