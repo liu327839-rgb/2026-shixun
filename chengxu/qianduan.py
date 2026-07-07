4ec2f946f68bb3c0a0c0ad924434ff7309aa68e4
liu <2979213874@qq.com>
2026-07-07 10:27:00 +0800

    5

--- chengxu/qianduan.py
+++ chengxu/qianduan.py
@@ -1,3 +1,10 @@
 from tkinter import *
 root = Tk()
+root.title("ai图片识别工具")
+root.geometry("1200x900+300+50")
+yongtu = Label(root, text="ai图片识别工具",font=("微软雅黑", 20), fg="black")
+yongtu.pack()
+renwu = Label(root, text="请在下方放入你所需要识别的图片",font=("微软雅黑", 15), fg="red")
+renwu.pack()
 root.mainloop()
+
