a105c366233766d1e990bc6e55537f7dd9ee52a0
ditto258 <2418515797@qq.com>
2026-07-08 10:48:20 +0800

    111

--- chengxu/im.py
+++ chengxu/im.py
@@ -1,11 +1,61 @@
 import cv2
 import numpy as np
-def process(image_path):
+def bilinear_resize(img,src_h,src_w,dst_h=224,dst_w=224):#scr:原图,dst:目标图
+   channels=img.shape
+   dst=np.zeros((dst_h,dst_w,channels),dtype=np.float32)
+   scale_x = src_w/dst_w
+   scale_y = src_h/dst_h
+   for c in range(channels):
+     for dst_x in range(dst_w):
+       for dst_y in range(dst_h):
+         src_x = (dst_x+0.5)*scale_x-0.5
+         src_y = (dst_y+0.5)*scale_y-0.5
+         #源图上临近点位置
+         src_x0 = int(np.floor(src_x))   
+         src_y0 = int(np.floor(src_y))
+         src_x1 = min(src_x0 + 1,src_w - 1)
+         src_y1 = min(src_y0 + 1,src_h - 1)
+         top = (src_x1 - src_x) * img[src_y0,src_x0,c] + (src_x - src_x0) * img[src_y0,src_x1,c]
+         bottom = (src_x1 - src_x) * img[src_y1,src_x0,c] + (src_x - src_x0) * img[src_y1,src_x1,c]
+         dst[dst_y,dst_x,c] = int((src_y1-src_y)*top+(src_y-src_y0)*bottom)
+   return dst
+                                
+def bgr_to_rgb(img):
+    dst = np.zeros_like(img)
+    h,w,c=img.shape
+    for i in range(h):
+      for j in range(w):
+         dst(i,j,2)=img(i,j,0)
+         dst(i,j,1)=img(i,j,1)
+         dst(i,j,0)=img(i,j,2)
+    return dst
+def manual_normalize(img):
+   h,w,c = img.shape
+   dst = np.zeros_like(img,dtype=np.float32)
+   for dst_h in range (h):
+      for dst_w in range (w):
+         for dst_c in range (c):
+            dst[dst_h,dst_w,dst_c]=img[dst_h,dst_w,dst_c]/255.0
+   return dst
+def manual_normalize(img):
+   mean = np.array([0.485,0.456,0.406],dtype=np.float32)
+   std = np.array([0.229,0.224,0.225],dtype=np.float32)
+
+   
+   
+   
+
+
+
+
+def imageprocess(image_path):
     img=cv2.imread(image_path)
-    img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    img=cv2.resive(img,(224,224))
-    img=img.astype(np.float32)/255.0
-    img=(img-[0.485,0.456,0.406])/[0.229,0.224,0.225]
+    #img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+    #img=cv2.resize(img,(224,224))
+
+    #img=img.astype(np.float32)/255.0
+    #img=(img-[0.485,0.456,0.406])/[0.229,0.224,0.225]
     return img.transpose(2,0,1)[np.newaxis,...]
 
 
